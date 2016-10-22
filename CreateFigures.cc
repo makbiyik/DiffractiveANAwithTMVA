f674a7b400600d3fd30f0a465f36eb6e5b047f45
EkuahCheck <EkuahCheck@users.noreply.github.com>
2016-10-22 18:37:38 +0200

    small changes for basic workflow

---
+++ CreateFigures.cc
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TStopwatch.h"
+
+
+
+int main( int argc, char** argv )
+{
+   TString methodList = ""; 
+
+   return 0; 
+}
